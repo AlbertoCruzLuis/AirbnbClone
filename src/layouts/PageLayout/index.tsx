8ce0ad2e2fc7ba8385e548da8136b8dec4696e0b
albertocruzluis <alu0101217734@ull.edu.es>
2022-09-22 15:53:13 +0000

    feat: add pagelayout

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -1,5 +1,8 @@
 import type { ReactNode } from "react";
 
+import { Header } from "@/components/Header";
+import { FooterSection } from "@/features/Footer/content/FooterSection";
+
 type IPageLayoutProps = {
   meta: ReactNode;
   children: ReactNode;
@@ -8,6 +11,8 @@ type IPageLayoutProps = {
 export const PageLayout = (props: IPageLayoutProps) => (
   <div>
     {props.meta}
-    {props.children}
+    <Header />
+    <main className="flex min-h-screen flex-col">{props.children}</main>
+    <FooterSection />
   </div>
 );
