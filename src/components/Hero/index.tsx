38e28e49e5f6a1ea31587859c805aaaa864ba1c5
albertocruzluis <alu0101217734@ull.edu.es>
2022-09-29 16:45:48 +0000

    feat: add HeroSection component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,23 @@
+import { SearchBarSection } from "@/features/SearchBar/content/SearchBarSection";
+
+import { CustomLink } from "../CustomLink";
+
+export const Hero = () => {
+  return (
+    <div className="mt-10 flex flex-col gap-12 p-8 xl:p-0">
+      <h1 className="leading-10">
+        <span className="text-5xl font-bold text-red-1000">Airbnb</span>
+        <div>
+          <span className="text-5xl font-light">Reserva</span>
+          <CustomLink className="text-5xl font-light" href="/">
+            {" alojamientos únicos"}
+          </CustomLink>
+          <span className="text-5xl font-light">
+            {" y descubre cada destino como un habitante más."}
+          </span>
+        </div>
+      </h1>
+      <SearchBarSection />
+    </div>
+  );
+};
